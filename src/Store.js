6607bd3c8c0852195954b90dc51b299a13cc2db0
asbillings07 <superswiftdeveloper@gmail.com>
2020-03-05 13:09:30 -0500

    inital commit

---
+++ src/Store.js
@@ -0,0 +1,20 @@
+import React, { createContext, useContext } from 'react'
+
+const Store = createContext()
+
+function Provider({ children }) {
+  const value = {
+    name: 'Aaron Billings'
+  }
+  return <Store.Provider value={value}>{children}</Store.Provider>
+}
+
+/* allows use of State without needing to repeat useContext hook,
+ also allows for easier testing */
+function useStore() {
+  const store = useContext(Store)
+  if (!store) throw new Error('Cannot `getStore` outside a context provider')
+  return store
+}
+
+export { Store, Provider, useStore }
